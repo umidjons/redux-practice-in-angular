60b162bfcd2886b41c66ee0a05f0e037b04eabae
Umid Almatov <almatov.us@gmail.com>
2017-10-01 14:18:14 +0500

    Improve reducer with Object.assign

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,7 +9,7 @@ export const INITIAL_STATE: IAppState = { counter: 0 };
 export function rootReducer(state: IAppState, action): IAppState {
   switch(action.type) {
       case INCREMENT:
-        return {counter: state.counter + 1};
+      return Object.assign({}, state, {counter: state.counter + 1});
   }
   return state;
 }
